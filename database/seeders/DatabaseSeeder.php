7bc0d6d6f9e3607f2a9e45a728a0dfec4c623981
AskarKadir <askarkadir07@gmail.com>
2023-05-23 20:17:21 +0700

    menambahkan category factory pada seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
 use App\Models\Todo;
 use Illuminate\Database\Seeder;
 use App\Models\User;
@@ -54,6 +56,7 @@ class DatabaseSeeder extends Seeder
 
 
         user::factory(100)->create();
+        Category::factory(100)->create();
         Todo::factory(500)->create();
     }
 }
