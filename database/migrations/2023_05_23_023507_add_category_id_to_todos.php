69ea18a34d58e4962714e5b7589f134f571b1268
AskarKadir <askarkadir07@gmail.com>
2023-05-23 20:16:07 +0700

    menghapus foreign key sebelum hapus tabel todos

--- database/migrations/2023_05_23_023507_add_category_id_to_todos.php
+++ database/migrations/2023_05_23_023507_add_category_id_to_todos.php
@@ -1,5 +1,6 @@
 <?php
 
+use GuzzleHttp\Psr7\DroppingStream;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -21,7 +22,9 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::table('todos', function (Blueprint $table) {
-            //
+            $table->dropForeign(['category_id']);
+            $table->dropColumn('category_id');
         });
+        Schema::dropIfExists('todos');
     }
 };
