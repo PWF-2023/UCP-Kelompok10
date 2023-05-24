e0c6cffc9653d5e7b85f13f974217c735b8b4963
AskarKadir <askarkadir07@gmail.com>
2023-05-24 21:10:49 +0700

    fix destroy route

--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,7 @@ Route::middleware('auth')->group(function () {
     Route::get('category', [CategoryController::class, 'index'])->name(('category.index'));
     Route::get('category/create', [CategoryController::class, 'create'])->name('category.create');
     Route::post('category', [CategoryController::class, 'store'])->name('category.store');
+    Route::delete('category/{category}', [CategoryController::class, 'destroy'])->name('category.destroy');
 
     Route::middleware('admin')->group(function () {
 
